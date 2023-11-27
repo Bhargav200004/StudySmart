193ea4484a6cd68e66a3cebe58b67d1c1a33bcaf
Bhargav200004 <Bhargav200004@gmail.com>
2023-11-28 01:35:24 +0530

    commit14(Creating Entity)

--- app/src/main/java/com/example/studysmart/domain/model/Subject.kt
+++ app/src/main/java/com/example/studysmart/domain/model/Subject.kt
@@ -1,17 +1,21 @@
 package com.example.studysmart.domain.model
 
 import androidx.compose.ui.graphics.Color
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.studysmart.ui.theme.gradient0
 import com.example.studysmart.ui.theme.gradient1
 import com.example.studysmart.ui.theme.gradient2
 import com.example.studysmart.ui.theme.gradient3
 import com.example.studysmart.ui.theme.gradient4
 
+@Entity
 data class Subject(
     val name : String,
     val goalHours : String,
     val colors : List<Color>,
-    val subjectId : Int
+    @PrimaryKey(autoGenerate = true)
+    val subjectId : Int? = null
 ){
     companion object{
         val subjectCardColor = listOf(
